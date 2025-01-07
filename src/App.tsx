7646b605c4643b592510be200ef0c1eee6042ced
Muhammad Imtiaz Hussain <imtiaz0307@gmail.com>
2025-01-07 23:06:56 +0500

    establish socket connection

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,15 @@
+import { useEffect, useMemo } from "react"
 import Router from "./routes"
+import { io } from "socket.io-client"
 
 const App = () => {
+  const socket = useMemo(() => io(import.meta.env.VITE_API_BASE_URL), [])
+
+  useEffect(() => {
+    socket.on("connect", () => {
+      console.log(socket.id)
+    })
+  }, [])
   return (
     <Router />
   )
